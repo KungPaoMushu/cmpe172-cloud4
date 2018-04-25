58aa2dc76243deff856d98dc08d0d35aa6eb7d5f
Will <jiepeng.huwu@sjsu.edu>
2018-04-24 19:07:36 -0700

    Third commit for Jenkins

--- jenkins-hw4/HelloCMPE172.java
+++ jenkins-hw4/HelloCMPE172.java
@@ -1,5 +1,6 @@
 public class HelloCMPE172 {
 	public static void main(String[] args) {
 		System.out.println("Initial setup for testing");
+		System.out.println("\nSecond print statement for Jenkins\n");
 	}
 }
